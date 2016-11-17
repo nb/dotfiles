651eb278ece42c23fdb495ef9ccdaab76b454878
Nikolay Bachiyski <nb@nikolay.bg>
2016-11-17 11:28:10 -0500

    Switching to Safari Technology Preview
    Nightly was too broken.

--- phoenix.js
+++ phoenix.js
@@ -103,7 +103,7 @@ api.bind( 'r', triple, function() {
 
 bindLaunch( '/', ['cmd'], 'iTerm' );
 bindLaunch( ';', ['cmd'], 'MacVim' );
-bindLaunch( '\'', ['cmd'], 'WebKit' );
+bindLaunch( '\'', ['cmd'], 'Safari Technology Preview' );
 bindLaunch( '\'', ['ctrl'], 'Firefox' );
 bindLaunch( ';', ['ctrl'], 'Google Chrome' );
 bindLaunch( '.', ['cmd'], 'Slack' );
