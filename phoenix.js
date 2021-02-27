33143611e82509ada9fab9cb21557cd1b63fc482
Nikolay Bachiyski <nb@nikolay.bg>
2021-02-27 23:19:41 +0200

    Style cleanup
    Spacing + vat/let/const

--- phoenix.js
+++ phoenix.js
@@ -4,122 +4,119 @@ function debug(message) {
 }
 
 function centerWindowInFrame(window, targetFrame) {
-    var windowFrame = window.frame(),
-      targetFrameCenter = {
-        x: targetFrame.x + targetFrame.width / 2,
-        y: targetFrame.y + targetFrame.height / 2,
-      };
-    window.setTopLeft({
-      x: targetFrameCenter.x - windowFrame.width / 2,
-      y: targetFrameCenter.y - windowFrame.height / 2,
-    });
+  const windowFrame = window.frame(),
+    targetFrameCenter = {
+      x: targetFrame.x + targetFrame.width / 2,
+      y: targetFrame.y + targetFrame.height / 2,
+    };
+  window.setTopLeft({
+    x: targetFrameCenter.x - windowFrame.width / 2,
+    y: targetFrameCenter.y - windowFrame.height / 2,
+  });
 }
 
 function maximizeWindowInFrame(window, targetFrame) {
-    window.setFrame(targetFrame);
-  }
-  
-  function windowFitsInFrame(window, targetFrame) {
-    var windowFrame = window.frame();
-    return (
-      windowFrame.width <= targetFrame.width &&
-      windowFrame.height <= targetFrame.height
-    );
-  }
-  
+  window.setFrame(targetFrame);
+}
+
+function windowFitsInFrame(window, targetFrame) {
+  const windowFrame = window.frame();
+  return (
+    windowFrame.width <= targetFrame.width &&
+    windowFrame.height <= targetFrame.height
+  );
+}
+
 function frameOfNextScreen(window) {
-    if (!window || !window.screen().next()) {
-      return;
-    }
-    return window
-      .screen()
-      .next()
-      .visibleFrame();
+  if (!window || !window.screen().next()) {
+    return;
+  }
+  return window.screen().next().visibleFrame();
 }
 
 const triple = ["ctrl", "alt", "cmd"];
 function bindLaunch(key, modifiers, appName) {
-    Key.on(key, modifiers, function() {
-        App.launch(appName).focus()
-    });
+  Key.on(key, modifiers, function () {
+    App.launch(appName).focus();
+  });
+}
+
+Key.on("c", triple, function () {
+  debug("Centering in screen");
+  const window = Window.focused(),
+    nextScreenFrame = window.screen().visibleFrame();
+  centerWindowInFrame(window, nextScreenFrame);
+});
+
+Key.on("space", triple, function () {
+  debug("Maximizing current window");
+  Window.focused().maximize();
+});
+
+Key.on("n", triple, function () {
+  debug("Center in next screen");
+  const window = Window.focused();
+  const nextScreenFrame = frameOfNextScreen(window);
+  if (!window || !nextScreenFrame) {
+    return;
   }
-
-  Key.on("c", triple, function() {
-    debug("Centering in screen");
-    const window = Window.focused(),
-      nextScreenFrame = window.screen().visibleFrame();
+  if (windowFitsInFrame(window, nextScreenFrame)) {
+    debug("fits!");
     centerWindowInFrame(window, nextScreenFrame);
-  });
+  } else {
+    maximizeWindowInFrame(window, nextScreenFrame);
+  }
+});
 
-  Key.on("space", triple, function() {
-    debug("Maximizing current window");
-    Window.focused().maximize();
-  });
+Key.on("w", triple, function () {
+  debug("Moving to top-left");
+  const window = Window.focused();
+  const screenFrame = window.screen().visibleFrame();
+  window.setTopLeft({ x: screenFrame.x, y: screenFrame.y });
+});
 
-  Key.on("n", triple, function() {
-    debug("Center in next screen");
-    var window = Window.focused(),
-      nextScreenFrame = frameOfNextScreen(window);
-    if (!window || !nextScreenFrame) {
-      return;
-    }
-    if (windowFitsInFrame(window, nextScreenFrame)) {
-      centerWindowInFrame(window, nextScreenFrame);
-    } else {
-      maximizeWindowInFrame(window, nextScreenFrame);
-    }
+Key.on("l", triple, function () {
+  debug("Moving to left-half");
+  const window = Window.focused(),
+    screenFrame = window.screen().visibleFrame();
+  window.setFrame({
+    x: screenFrame.x,
+    y: screenFrame.y,
+    width: screenFrame.width / 2,
+    height: screenFrame.height,
   });
+});
 
-  Key.on("w", triple, function() {
-    debug("Moving to top-left");
-    var window = Window.focused(),
-      screenFrame = window.screen().visibleFrame();
-    window.setTopLeft({ x: screenFrame.x, y: screenFrame.y });
+Key.on("r", triple, function () {
+  debug("Moving to right-half");
+  const window = Window.focused();
+  const screenFrame = window.screen().visibleFrame();
+  window.setFrame({
+    x: screenFrame.x + screenFrame.width / 2,
+    y: screenFrame.y,
+    width: screenFrame.width / 2,
+    height: screenFrame.height,
   });
+});
 
-  Key.on("l", triple, function() {
-    debug("Moving to left-half");
-    const window = Window.focused(),
-      screenFrame = window.screen().visibleFrame();
-    window.setFrame({
-      x: screenFrame.x,
-      y: screenFrame.y,
-      width: screenFrame.width / 2,
-      height: screenFrame.height,
-    });
-  });
-
-  Key.on("r", triple, function() {
-    debug("Moving to right-half");
-    var window = Window.focused(),
-      screenFrame = window.screen().visibleFrame();
-    window.setFrame({
-      x: screenFrame.x + screenFrame.width / 2,
-      y: screenFrame.y,
-      width: screenFrame.width / 2,
-      height: screenFrame.height,
-    });
-  });
-
-  bindLaunch("/", ["cmd"], "iTerm");
-  bindLaunch(";", ["cmd"], "Visual Studio Code");
-  bindLaunch("'", ["cmd"], "Safari");
-  bindLaunch("'", ["ctrl"], "Firefox");
-  bindLaunch(";", ["ctrl"], "Google Chrome Canary");
-  bindLaunch(".", ["cmd"], "Slack");
-  bindLaunch(".", ["cmd", "shift"], "Skype");
-  bindLaunch(".", ["cmd", "ctrl"], "LimeChat");
-  bindLaunch(";", ["cmd", "ctrl"], "Calendar");
-  bindLaunch("\\", ["cmd", "ctrl"], "Simplenote");
-  bindLaunch("\\", ["cmd", "shift"], "zoom.us");
-  bindLaunch("'", ["cmd", "ctrl"], "Mail");
-  bindLaunch("'", ["cmd", "shift"], "Mailplane");
-  bindLaunch("\\", ["cmd"], "Music");
-  bindLaunch("m", triple, "MPlayer OSX Extended");
-  bindLaunch("k", ["cmd", "ctrl"], "Keynote");
-  bindLaunch("x", ["cmd", "ctrl"], "GitX");
-  bindLaunch("p", ["cmd", "ctrl"], "WordPress.com");
-  bindLaunch("t", ["cmd", "ctrl"], "Telegram");
-  bindLaunch("d", ["cmd", "ctrl"], "Deckset");
-  bindLaunch("r", ["cmd", "ctrl"], "Roam Research");
-  
+bindLaunch("/", ["cmd"], "iTerm");
+bindLaunch(";", ["cmd"], "Visual Studio Code");
+bindLaunch("'", ["cmd"], "Safari");
+bindLaunch("'", ["ctrl"], "Firefox");
+bindLaunch(";", ["ctrl"], "Google Chrome Canary");
+bindLaunch(".", ["cmd"], "Slack");
+bindLaunch(".", ["cmd", "shift"], "Skype");
+bindLaunch(".", ["cmd", "ctrl"], "LimeChat");
+bindLaunch(";", ["cmd", "ctrl"], "Calendar");
+bindLaunch("\\", ["cmd", "ctrl"], "Simplenote");
+bindLaunch("\\", ["cmd", "shift"], "zoom.us");
+bindLaunch("'", ["cmd", "ctrl"], "Mail");
+bindLaunch("'", ["cmd", "shift"], "Mailplane");
+bindLaunch("\\", ["cmd"], "Music");
+bindLaunch("m", triple, "MPlayer OSX Extended");
+bindLaunch("k", ["cmd", "ctrl"], "Keynote");
+bindLaunch("x", ["cmd", "ctrl"], "GitX");
+bindLaunch("p", ["cmd", "ctrl"], "WordPress.com");
+bindLaunch("t", ["cmd", "ctrl"], "Telegram");
+bindLaunch("d", ["cmd", "ctrl"], "Deckset");
+bindLaunch("r", ["cmd", "ctrl"], "Roam Research");
